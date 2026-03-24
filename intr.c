5c946a8d0058b3d0d9a4a9cc0e8fa177c4a3af13
oukgit2026 <kogulcanumut100@gmail.com>
2026-03-24 17:35:27 +0300

    Update intr.c

--- intr.c
+++ intr.c
@@ -7,17 +7,29 @@ to do
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char* argv[]){
-    
+
     /* format
     ./<program> capital interestRate_Yearly period cycle
     ./intr 10000 40.5 32 6
     */
-    float capital = argv[0];
-    float interesestRate_Yearly = argv[1];
-    int periodDays = argv[2];
-    int cycle = argv[3];
     
+    // define variables
+    float capital;
+    float interesestRate_Yearly;
+    int periodDays;
+    int cycle;
+    
+    // assign command line arg to variables
+    capital = atof(argv[1]);
+    interesestRate_Yearly = atof(argv[2]);
+    periodDays = atoi(argv[3]);
+    cycle = atoi(argv[4]);
+
+    /* debug */
+    printf("\n\tcapital\t: %f", capital);
+    printf("\n");
     return 0;
 }
